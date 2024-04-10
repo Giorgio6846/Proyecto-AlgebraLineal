063f71f0ef611dbd2a0c4f4fd8d89cb8f8de33a5
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2024-04-10 13:07:10 -0500

    TestChanges

--- src/Screens/Result.py
+++ src/Screens/Result.py
@@ -1,8 +1,22 @@
-from PySide6.QtWidgets import QWidget, QLabel
+from PySide6.QtWidgets import (
+    QWidget,
+    QLabel,
+    QVBoxLayout,
+    QSizePolicy,
+    QHBoxLayout,
+    QPushButton,
+    QInputDialog,
+    QFormLayout)
+
+from PySide6.QtGui import QFont
+from PySide6.QtCore import *
+import random
 
 class Result(QWidget):
 
     def __init__(self, data):
         super().__init__()
         self.dataApp = data
+        
+        
 
